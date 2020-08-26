2d78d1f6d0bcef04bca05d0d9c505e1749488750
pratibha goyal <erpratibhagoyal@gmail.com>
2020-08-26 03:51:01 -0400

    second commit

--- myproject/src/Example.java
+++ myproject/src/Example.java
@@ -5,8 +5,10 @@ public static void main(String [] args)
 	String name="Pratibha Goyal";
 	String email="erpratibhagoyal@gmail.com";
 	int a=10123456;
+	int b=10;
 	boolean student=true;
 	System.out.println(a);
+	System.out.println(b);
 	System.out.println(name);
 	System.out.println(student);
 }
