f4a8a8252739834c425279cd944b7320c68bf9b3
pratibha goyal <erpratibhagoyal@gmail.com>
2020-08-27 01:25:56 -0400

    updated example

--- myproject/src/Example.java
+++ myproject/src/Example.java
@@ -6,6 +6,7 @@ public static void main(String [] args)
 	String email="erpratibhagoyal@gmail.com";
 	int a=10123456;
 	int b=10;
+	int c=0;
 	boolean student=true;
 	System.out.println(a);
 	System.out.println(b);
